946546a42369227dde861551bd60a54ef029e57b
Sylvain Girault <syl@djro.fr>
2026-06-19 16:20:42 +0200

    feat(i18n) : separate namespaces

--- src/shared/i18n/i18n.ts
+++ src/shared/i18n/i18n.ts
@@ -2,17 +2,23 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import fr from './locales/fr.json'
+import frYams from './locales/fr-yams.json'
 import en from './locales/en.json'
+import enYams from './locales/en-yams.json'
+
+console.log('FR loaded:', fr)  
+console.log('EN loaded:', en)  
 
 i18n
   .use(initReactI18next)
   .init({
     resources: {
-      fr: { translation: fr },
-      en: { translation: en },
+      fr: { translation: fr, yams : frYams },
+      en: { translation: en, yams : enYams  },
     },
     lng: 'fr',
     fallbackLng: 'en',
+    defaultNS: 'translation', 
     interpolation: { escapeValue: false },
   })
 

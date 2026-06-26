5784da894cde2be99545a7f94753e59079022ed8
Sylvain Girault <syl@djro.fr>
2026-06-26 08:23:03 +0200

    security: move firebase credentials to .env

--- src/shared/firebase/config.ts
+++ src/shared/firebase/config.ts
@@ -4,13 +4,13 @@ import { getDatabase } from "firebase/database"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAD6tvCAnnMgFTwNyizmqTUer6-nc6SKDE",
-  authDomain: "jeuxgratis-116b3.firebaseapp.com",
-  projectId: "jeuxgratis-116b3",
-  databaseURL: "https://jeuxgratis-116b3-default-rtdb.europe-west1.firebasedatabase.app", 
-  storageBucket: "jeuxgratis-116b3.firebasestorage.app",
-  messagingSenderId: "321489028962",
-  appId: "1:321489028962:web:d1478d0b8f352aa4e2ce3b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase

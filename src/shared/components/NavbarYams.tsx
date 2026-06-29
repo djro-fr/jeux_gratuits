e6899c5af20a20f8671fd538da901bf2b72f069f
Sylvain Girault <syl@djro.fr>
2026-06-29 18:53:41 +0200

    feat(yams/ui): fix restart yams in navbar

--- src/shared/components/NavbarYams.tsx
+++ src/shared/components/NavbarYams.tsx
@@ -1,43 +1,45 @@
-
-import { LeaderboardModal } from '@/features/games/yams/ui/components/LeaderboardModal'
-import { useYamsGame } from '@/features/games/yams/ui/hooks/useYamsGame'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { IconsSprite } from './IconsSprite'
+import { useState } from 'react'
+import { LeaderboardModal } from '@/features/games/yams/ui/components/LeaderboardModal'
 
 export const NavbarYams = () => {
-  const { handleRestart } = useYamsGame()
+  const navigate = useNavigate()  
   const [isLeaderboardOpen, setIsLeaderboardOpen] = useState(false)
-
+  
+  const handleRestart = () => {
+    navigate(0)  
+  }
+  
   return (    
     <>
-    <nav id="navbar" className='py-2.5'>
-      <div className="container mx-auto flex">
-        <ul className="flex justify-between w-full px-5">
-          <li>
-            <Link to="/">
-            <button className='icon' onClick={handleRestart}>
-              <IconsSprite value='home'/>              
-            </button>
-            </Link>
-          </li>
-          <li>
-            <button className='icon' onClick={handleRestart}>
-              <IconsSprite value='restart'/>              
-            </button>
-          </li>
-          <li>
-            <button className='icon' onClick={() => setIsLeaderboardOpen(true)}>
-              <IconsSprite value='leaderboard'/>              
-            </button>
-          </li>
-        </ul>
-      </div>   
-    </nav>
-    <LeaderboardModal
-      isOpen={isLeaderboardOpen}
-      onClose={() => setIsLeaderboardOpen(false)}
-    />
+      <nav id="navbar" className='py-2.5'>
+        <div className="container mx-auto flex">
+          <ul className="flex justify-between w-full px-5">
+            <li>
+              <Link to="/">
+                <button className='icon'>
+                  <IconsSprite value='home'/>              
+                </button>
+              </Link>
+            </li>
+            <li>
+              <button className='icon' onClick={handleRestart}>
+                <IconsSprite value='restart'/>              
+              </button>
+            </li>
+            <li>
+              <button className='icon' onClick={() => setIsLeaderboardOpen(true)}>
+                <IconsSprite value='leaderboard'/>              
+              </button>
+            </li>
+          </ul>
+        </div>   
+      </nav>
+      <LeaderboardModal
+        isOpen={isLeaderboardOpen}
+        onClose={() => setIsLeaderboardOpen(false)}
+      />
     </>
-    )
+  )
 }

c6f3b12c6b4cd673f330c0baa88d24968a96db66
Sylvain Girault <syl@djro.fr>
2026-07-01 14:18:05 +0200

    feat(yams/ui): add picture for splash screen home

--- src/ui/pages/HomePage.tsx
+++ src/ui/pages/HomePage.tsx
@@ -5,10 +5,10 @@ export const HomePage = () => {
   const { t } = useTranslation()
   const { t: tYams } = useTranslation('yams')
   return (
-    <div className='w-full flex flex-col justify-center items-center'>
+    <div className='w-full flex flex-col justify-center items-center mb-5'>
       <h1 className='mb-2 text-5xl'>{t('games')}</h1>
-      <img src="../assets/dicesplash.jpg" className='w-full mt-1 mb-5' alt="" />
-      <div className='flex w-full justify-center'>
+      <img src="../assets/dicesplash.jpg" className='w-full max-w-150 mt-1 mb-5' alt="" />
+      <div className='flex w-full justify-center mb-10'>
         <Link to="/game/yams">{tYams('title')}</Link>
       </div>
     </div>
